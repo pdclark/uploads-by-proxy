f05280ab62d3419639f674ad6146476a50ba31d3
Paul Clark <pdclark@pdclark.com>
2012-09-23 02:51:35 -0800

    Option to load by local stream only

---
+++ class-get-public-ip.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Work around local DNS by asking public web sites to look up the IP of a domain
+ */
+class Storm_Get_Public_IP {
+
+	var $domain;
+	var $transient;
+	var $expire = 86400;
+	var $ip;
+	var $ip_pattern = '/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/';
+
+	function __construct( $domain ) {
+		$this->domain = $domain;
+		$this->transient = 'dbp_'.$domain;
+
+		$ip = get_transient( $this->transient );
+
+		if ( empty($ip) && '127.0.0.1' == $_SERVER['SERVER_ADDR'] && '127.0.0.1' == $_SERVER['REMOTE_ADDR'] ) {
+			// Loading and serving from localhost
+			$custom_url = apply_filters('ubp_ip_url', false, $this->domain );
+			$custom_args = apply_filters('ubp_ip_args', array() );
+
+			if ( $custom_url ) $ip = $this->get_ip( $custom_url, $custom_args );
+			if ( !$ip ) $ip = $this->get_ip( "http://baremetal.com/cgi-bin/dnsip?target=$domain" );
+			if ( !$ip ) $ip = $this->get_ip( 'http://hostnametoip.com/', array( 'index'=>1, 'method' => 'POST', 'referer'=>'http://hostnametoip.com/', 'body' => 'conversion=1&addr='.$domain ) );
+			if ( !$ip ) $ip = $this->get_ip( "http://aruljohn.com/cgi-bin/hostname2ip.pl?host=$domain", array( 'referer'=>'http://aruljohn.com/hostname2ip.html' ) );
+
+			if ( $ip ) {
+				set_transient( $this->transient, $ip, $this->expire );
+				$this->ip = $ip;
+			}
+		}
+
+		if ( empty($ip) ){ $ip = $domain; }
+
+		$this->ip = $ip;
+	}
+
+	function __toString() {
+		return $this->ip;
+	}
+
+	public function get_ip( $url, $args ) {
+		$defaults = array( 
+			'method' => 'GET',
+			'referer'=> $domain,
+			'body' => '',
+			'index' => 0,
+		);
+		$args = wp_parse_args($args, $defaults);
+		extract( $args );
+
+		$query_args = array(
+			'method' => $method,
+			'headers' => array('Referer'=>$referer),
+			'body' => $body,
+		);
+
+		$response = wp_remote_get( $url, $query_args );
+		$body = strip_tags($response['body']);
+
+		preg_match_all( $this->ip_pattern, $body, $matches );
+
+		FB::log($body, '$body');
+		FB::log($matches, '$matches');
+		exit;
+
+		if ( !empty($matches[0][$index] ) ) { return $matches[0][$index]; }
+
+		return false;
+
+	}
+
+}
--- uploads-by-proxy.php
+++ uploads-by-proxy.php
@@ -30,13 +30,38 @@ Author URI: http://brainstormmedia.com
  * **********************************************************************
  */
 
+/*
+	Settings & Filters:
+	// Set up a proxy in /wp-config.php
+	// Disable & Enable plugin after putting in theme or plugin
+	define('UBP_PROXY', 'proxy.domain.com' );
+
+	// Set up a proxy in /wp-content/THEME/functions.php
+	// Disable & Enable plugin after putting in theme or plugin
+	add_filter('ubp_proxy', create_function('', 'return "proxy.website.com";'));
+
+	// Force disable of .htaccess rewrite rules in /wp-content/THEME/functions.php
+	// Disable & Enable plugin after putting in theme or plugin
+	add_filter('ubp_remove_rewrite_rules', '__return_true');
+
+	// Add a custom source for IP address lookups
+	add_filter('ubp_ip_url', 'ubp_ip_url', 10, 2);
+	function ubp_ip_url( $url, $domain ) {
+		return 'http://website.com/ip.php?domain='.$domain;
+	}
+*/
+
+
 /**
- * Domain running an apache passthrough proxy, using this .htaccess:
- *     # Proxy requests from /domain/path to http://domain/path
- *     <IfModule mod_rewrite.c>
- *     RewriteEngine On
- *     RewriteRule ^([^/]*)/(.*)$ http://$1/$2 [P,L]
- *     </IfModule>
+ * Handle redirection with Apache rewrite rules
+ * Requires UBP_PROXY point to a domain that had mod_rewrite and mod_proxy enabled.
+ * Put the contents of htaccess-remote-proxy.txt in the remote root directory as .htaccess
+ * Then put the address of your proxy in wp-config.php, like:
+ *     define('UBP_PROXY', 'proxy.domain.com' );
+ * 
+ * Remote Proxy:   On
+ * Local Rewrites: On
+ * Speed:          Very Fast
  */
 if ( !defined('UBP_PROXY') ) define('UBP_PROXY', false); // e.g., proxy.domain.com
 
@@ -45,13 +70,24 @@ $storm_uploads_by_proxy = new Storm_Uploads_by_Proxy();
 class Storm_Uploads_by_Proxy {
 
 	var $marker = 'Uploads by Proxy';
+	var $expire = 86400;
+	var $proxy;
+	var $domain;
 
 	function __construct() {
+
 		register_activation_hook(   __FILE__, array($this, 'save_mod_rewrite_rules') ); 
 		register_deactivation_hook( __FILE__, array($this, 'remove_rewrite_rules') ); 
+
 		add_action('template_redirect', array($this, 'template_redirect'));
 	}
 
+	/**
+	 * Handle redirection through WordPress 404
+	 * 
+	 * Remote Proxy:   On/Off
+	 * Local Rewrites: Off
+	 */
 	function template_redirect(){
 		// Only run on 404
 		if ( !is_404() ) { return; }
@@ -62,10 +98,16 @@ class Storm_Uploads_by_Proxy {
 		if ( !$this->allow_path($path) ) { return; }
 
 		$this->redirect( $path );
+
+		$this->stream( $path );
 	}
 
 	/**
-	 * Redirect to live file through proxy
+	 * Redirect files to remote proxy
+	 * 
+	 * Remote Proxy:   On
+	 * Local Rewrites: Off
+	 * Speed:          Slow
 	 */
 	public function redirect( $path ) {
 		if ( !$this->get_proxy() ) { return false; }
@@ -74,10 +116,41 @@ class Storm_Uploads_by_Proxy {
 		status_header( 200 );
 		$wp_query->is_404 = false;
 
-		$url = 'http://' . trailingslashit($this->get_proxy()) . $this->get_domain() . $path;
-
-		header( "Location: $url", 302 );
+		header( 'Location: ' . $this->get_url( $path ), 302 );
 		exit;
+	}
+
+	/**
+	 * Stream files from publicly registered IP address through PHP
+	 * 
+	 * Remote Proxy:   Off
+	 * Local Rewrites: Off
+	 * Speed:          Very Slow
+	 */
+	public function stream( $path ) {
+		require dirname(__FILE__).'/class-get-public-ip.php';
+
+		$ip = new Storm_Get_Public_IP( $this->get_domain() );
+
+		// Tell the remote IP that we're loading for a specific domain
+		$args = array( 'headers' => array( 'Host' => $this->get_domain() ) );
+		$url = 'http://'.$ip.$path;
+
+		$response = wp_remote_get( $url, $args);
+
+		if ( !is_wp_error($response) && 200 == $response['response']['code'] ) {
+			global $wp_query;
+			status_header( 200 );
+			$wp_query->is_404 = false;
+
+			foreach( $response['headers'] as $name => $value ){
+				header( "$name: $value" );
+			}
+
+			echo $response['body'];
+
+			exit;
+		}
 	}
 
 	public function get_rewrite_rules() {
@@ -137,6 +210,10 @@ class Storm_Uploads_by_Proxy {
 	public function get_domain() {
 		if( !isset($this->domain) ){ $this->domain = apply_filters( 'ubp_domain', $_SERVER['HTTP_HOST'] ); }
 		return $this->domain;
+	}
+
+	public function get_url($path) {
+		return 'http://' . trailingslashit( $this->get_proxy() ) . $this->get_domain() . $path;
 	}
 
 	/**
