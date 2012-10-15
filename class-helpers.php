5f65b2c6b6788dbd369e498985f098a081d477f1
Paul Clark <pdclark@brainstormmedia.com>
2012-10-14 17:37:49 -0800

    Require single-site

--- class-helpers.php
+++ class-helpers.php
@@ -14,14 +14,39 @@ class UBP_Helpers {
 	}
 
 	static public function requirements_check() {
-		self::permalinks_enabled();
-		self::uploads_writable();
+		// Deactivates if multisite, so run immediately
+		self::require_no_multisite();
+
+		add_action( 'admin_notices', 'UBP_Helpers::request_uploads_writable' );
+		add_action( 'admin_notices', 'UBP_Helpers::request_permalinks_enabled' );
+	}
+
+	/**
+	 * Require single-site install before activating.
+	 */
+	function require_no_multisite() {
+		if ( function_exists( 'is_multisite' ) && !is_multisite() ) { return true; }
+
+		if ( is_admin() && (!defined('DOING_AJAX') || !DOING_AJAX) ) {
+			require_once ABSPATH.'/wp-admin/includes/plugin.php';
+			deactivate_plugins( UBP_PLUGIN_FILE );
+			wp_die( __( 'Uploads by Proxy is not compatible with network installs. Please activate the plugin on single-site installs only.', 'uploads-by-proxy') );
+		} else {
+			return false;
+		}
+
+		echo '<div id="ubp_uploads_message" class="error"><p>'
+			 . __( 'The uploads directory must be enabled for Uploads by Proxy to work. ', 'uploads-by-proxy' )
+			 . sprintf( __( '%sRead about changing file permissions%s, or run:<br/><code>chmod 755 "%s";', 'uploads-by-proxy' ), '<a href="http://codex.wordpress.org/Changing_File_Permissions" target="_blank">', '</a>', $upload_dir['basedir'] )
+			 . '</p></div>';
+
+		return false;
 	}
 
 	/**
 	 * Display an error message when permalinks are disabled
 	 */
-	static public function permalinks_enabled() {
+	static public function request_permalinks_enabled() {
 		if ( '' != get_option('permalink_structure') ) { return true; }
 
 		echo '<div id="ubp_permalinks_message" class="error"><p>'
@@ -35,7 +60,7 @@ class UBP_Helpers {
 	/**
 	 * Display an error message when uploads folder is not writable
 	 */
-	static public function uploads_writable() {
+	static public function request_uploads_writable() {
 		$upload_dir = wp_upload_dir();
 		if ( is_writable( $upload_dir['basedir'] ) ) { return true; }
 
@@ -47,8 +72,4 @@ class UBP_Helpers {
 		return false;
 	}
 
-	function warn_if_network() {
-		if ( function_exists( 'is_multisite' ) && is_multisite() )
-			return;
-	}
 }
--- uploads-by-proxy.php
+++ uploads-by-proxy.php
@@ -52,6 +52,7 @@ if ( !defined('UBP_LIVE_DOMAIN') ) define('UBP_LIVE_DOMAIN', $_SERVER['HTTP_HOST
  */
 if ( !defined('UBP_IS_LOCAL') ) define('UBP_IS_LOCAL', ( '127.0.0.1' == $_SERVER['SERVER_ADDR'] && '127.0.0.1' == $_SERVER['REMOTE_ADDR'] ) );
 
+define( 'UBP_PLUGIN_FILE', __FILE__ );
 
 /**
  * Check for PHP 5.2 or higher before activating.
@@ -59,7 +60,7 @@ if ( !defined('UBP_IS_LOCAL') ) define('UBP_IS_LOCAL', ( '127.0.0.1' == $_SERVER
 if ( version_compare(PHP_VERSION, '5.2', '<') ) {
 	if ( is_admin() && (!defined('DOING_AJAX') || !DOING_AJAX) ) {
 		require_once ABSPATH.'/wp-admin/includes/plugin.php';
-		deactivate_plugins( __FILE__ );
+		deactivate_plugins( UBP_PLUGIN_FILE );
 		wp_die( sprintf( __( 'Uploads by Proxy requires PHP 5.2 or higher, as does WordPress 3.2 and higher. The plugin has now disabled itself. For information on upgrading, %ssee this article%s.', 'uploads-by-proxy'), '<a href="http://codex.wordpress.org/Switching_to_PHP5" target="_blank">', '</a>') );
 	} else {
 		return;
@@ -71,7 +72,7 @@ require_once dirname( __FILE__ ).'/class-helpers.php';
 // Only initialize if we're on a development server and have a 404
 if ( UBP_IS_LOCAL ) {
 
-	add_action( 'admin_notices', 'UBP_Helpers::requirements_check' );
+	UBP_Helpers::requirements_check();
 	add_filter( '404_template', 'UBP_Helpers::frontend_init' );
 
 }
